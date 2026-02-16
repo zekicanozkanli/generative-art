6534fc53dc843df8c0f47821fe3f70f5e23a7d59
zekicanozkanli <117727608+zekicanozkanli@users.noreply.github.com>
2026-02-16 17:58:00 +0300

    Auto-Sync: last update: Feb 16, 17:58

--- yin_and_yang/anim.js
+++ yin_and_yang/anim.js
@@ -35,7 +35,7 @@ function draw() {
 
         let d = mag(k, e) + cos(e + t * 2 + (i % 2) * 4);
 
-        let r = y * k / 5 * (2 + sin(d * 2 + y - t * 4)) + 80;
+        let r = j * k / 5 * (2 + sin(d * 2 + j - t * 4)) + 80;
 
         let theta = d / 4 - t / 2 + (i % 2) * 3;
         point(r * cos(theta) + 400, r * sin(theta) + 200);
