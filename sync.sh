f5ae817810848a24744c1bc796bbbcd6f2d64b1d
zekicanozkanli <117727608+zekicanozkanli@users.noreply.github.com>
2026-02-16 14:11:27 +0300

    Auto-Sync: last update: Feb 16, 14:11

--- sync.sh
+++ sync.sh
@@ -11,6 +11,5 @@ git pull --rebase origin main
 if ! git diff-index --quiet HEAD; then
     git commit -m "Auto-Sync: last update: $(date '+%b %d, %H:%M')"
     git push origin main
-    # Success notification
     osascript -e 'display notification "generative-art synced to GitHub" with title "Git Auto" subtitle "Success"'
 fi
