3e82c438bb84e06bdd1862d76c2bacc6190bb3af
zekicanozkanli <117727608+zekicanozkanli@users.noreply.github.com>
2026-02-16 13:19:50 +0300

    Auto-Sync: last update: Feb/16, 13:19

---
+++ elements/m_.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(800, 800);
+    background(9);
+    stroke(255);
+    strokeWeight(2);
+    noFill();
+}
+
+let t = 0;
+
+function draw() {
+    background(9);
+    translate(400, 400);
+
+    t += 0.01;
+
+    for (let i = 0; i < 100; i += 0.01) {
+
+        let m = 250
+
+        let r = m * (100 % noise(i ** t));
+
+        let x = r * cos(i);
+        let y = r * sin(i);
+
+        point(x, y);
+    }
+
+}
